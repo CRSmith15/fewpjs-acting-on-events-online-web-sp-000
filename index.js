fca5b4917e5a78049ecfff382da649f2af94256c
Chance Smith <chancesmith97@hotmail.com>
2020-09-05 22:30:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ function moveDodgerLeft() {
   }
 }
 
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
 function moveDodgerRight() {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
